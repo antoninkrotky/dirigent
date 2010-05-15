410ea888ef70a463b43cbb0b364f1a9716e62626
bfelipee <bfelipee@0be835e6-de77-11de-b357-fb893c5a43fa>
2010-05-15 14:21:19 +0000

    resolving merge confilcts

--- trunk/dirigent/src/org/dirigent/kettle/JobEntryDirigentPlugin.java
+++ trunk/dirigent/src/org/dirigent/kettle/JobEntryDirigentPlugin.java
@@ -4,14 +4,13 @@ import java.util.List;
 
 import org.dirigent.Dirigent;
 import org.dirigent.config.DirigentConfig;
-import org.dirigent.metafacade.builder.csv.MappingDao;
-import org.dirigent.metafacade.builder.vo.MappingVO;
 import org.pentaho.di.cluster.SlaveServer;
 import org.pentaho.di.core.Result;
 import org.pentaho.di.core.database.DatabaseMeta;
 import org.pentaho.di.core.exception.KettleDatabaseException;
 import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.di.core.exception.KettleXMLException;
+import org.pentaho.di.core.logging.LogChannel;
 import org.pentaho.di.core.xml.XMLHandler;
 import org.pentaho.di.job.entry.JobEntryBase;
 import org.pentaho.di.job.entry.JobEntryInterface;
@@ -22,18 +21,11 @@ import org.w3c.dom.Node;
 public class JobEntryDirigentPlugin extends JobEntryBase implements Cloneable,
 		JobEntryInterface {
 
-	// private static final String STEP_NAME = "stepName";
 	private static final String MODEL = "model";
-//	private static final String OUTPUT = "output";
 	private static final String URI = "uri";
-	// private static final String ENCLOSURE = "enclosure";
-
-	// private String stepName;
 	private String model;
-//	private String output;
 	private String uri;
-
-	// private String enclosure;
+	private LogChannel log = new LogChannel(this);
 
 	public JobEntryDirigentPlugin(String n) {
 		super(n, "");
@@ -49,8 +41,6 @@ public class JobEntryDirigentPlugin extends JobEntryBase implements Cloneable,
 		return je;
 	}
 
-	
-	
 	public String getModel() {
 		return model;
 	}
@@ -67,15 +57,6 @@ public class JobEntryDirigentPlugin extends JobEntryBase implements Cloneable,
 		this.uri = uri;
 	}
 
-//	public String getOutput() {
-//		return output;
-//	}
-//
-//	public void setOutput(String output) {
-//		this.output = output;
-//	}
-
-	
 	public Object getValue() {
 		return null;
 	}
@@ -87,11 +68,7 @@ public class JobEntryDirigentPlugin extends JobEntryBase implements Cloneable,
 		retval.append(super.getXML());
 
 		retval.append("      " + XMLHandler.addTagValue(MODEL, model));
-//		retval.append("      " + XMLHandler.addTagValue(OUTPUT, output));
 		retval.append("      " + XMLHandler.addTagValue(URI, uri));
-		// retval.append("      " + XMLHandler.addTagValue(ENCLOSURE,
-		// enclosure));
-
 		return retval.toString();
 	}
 
@@ -102,9 +79,7 @@ public class JobEntryDirigentPlugin extends JobEntryBase implements Cloneable,
 		try {
 			super.loadXML(entrynode, databases, slaveServers);
 			model = XMLHandler.getTagValue(entrynode, MODEL);
-//			output = XMLHandler.getTagValue(entrynode, OUTPUT);
 			uri = XMLHandler.getTagValue(entrynode, URI);
-			// enclosure = XMLHandler.getTagValue(entrynode, ENCLOSURE);
 		} catch (KettleXMLException xe) {
 			throw new KettleXMLException(
 					"Unable to load file exists job entry from XML node", xe);
@@ -118,10 +93,7 @@ public class JobEntryDirigentPlugin extends JobEntryBase implements Cloneable,
 		try {
 			super.loadRep(rep, id_jobentry, databases, slaveServers);
 			model = rep.getJobEntryAttributeString(id_jobentry, MODEL);
-//			output = rep.getJobEntryAttributeString(id_jobentry, OUTPUT);
 			uri = rep.getJobEntryAttributeString(id_jobentry, URI);
-			// enclosure = rep.getJobEntryAttributeString(id_jobentry,
-			// ENCLOSURE);
 
 		} catch (KettleException dbe) {
 			throw new KettleException(
@@ -135,9 +107,7 @@ public class JobEntryDirigentPlugin extends JobEntryBase implements Cloneable,
 		try {
 			super.saveRep(rep, id_job);
 			rep.saveJobEntryAttribute(id_job, getObjectId(), MODEL, model);
-//			rep.saveJobEntryAttribute(id_job, getObjectId(), OUTPUT, output);
 			rep.saveJobEntryAttribute(id_job, getObjectId(), URI, uri);
-	
 
 		} catch (KettleDatabaseException dbe) {
 			throw new KettleException(
@@ -151,37 +121,31 @@ public class JobEntryDirigentPlugin extends JobEntryBase implements Cloneable,
 
 		Result result = new Result(nr);
 		result.setResult(false);
-		logDetailed(toString(), "Starting DIRIGENT Job ");
+		log.logDetailed("Starting DIRIGENT Job ");
 
-		 
-//		try {
-//			DirigentTransBuilder transbuilder = new DirigentTransBuilder();
-			try {
-//				MappingVO mapping = new MappingDao().getMapping(uri);
-//				String schemaUri = mapping.schemaUri;
-//				String pattern = mapping.pattern;
-//				Collection<MappingSourceTableVO> sourceTableMapping = mapping.mappingSourceTables;
-//				String target = mapping.targetTableUri;
-				
-//				transbuilder.createDBConnection(new SchemaDao().getSchema(schemaUri));
-				
-				System.setProperty(DirigentConfig.MODEL_PATH, model);
-				System.setProperty("dirigent.model.type", "EA");
-				String[] args = {model, uri};
-				Dirigent.main(args);
-				
-			} catch (NullPointerException npe) {
-				logDetailed(toString(), "Exception encountered: "
-						+ npe.getMessage());
-				result.setResult(false);
-				
-			}
-//			transbuilder.finish(output);
-//		} catch (Exception e) {
-//			logDetailed(toString(), "Error while executing job. "
-//					+ e.getMessage());
-//			result.setResult(false);
-//		}
+		try {
+
+			System.setProperty(DirigentConfig.MODEL_PATH, model);
+			System.setProperty("dirigent.model.type", "EA");
+			String[] args = { model, uri };
+			Dirigent.main(args);
+
+		} catch (NullPointerException npe) {
+			logDetailed(toString(), "Exception encountered: "
+					+ npe.getMessage());
+			result.setResult(false);
+			return result;
+
+		} catch (RuntimeException rex) {
+			log
+					.logDetailed("RunTime Exception encountered. A problem with model? This maybe an error in your database/schema settings as a result of SQLException. Message: \n"
+							+ rex.getMessage());
+			log.logDetailed("Finishing DIRIGENT Job with no success");
+			result.setResult(false);
+			result.setNrErrors(result.getNrErrors() + 1);
+			return result;
+		}
+
 		result.setResult(true);
 		logDetailed(toString(), "Finishing DIRIGENT Job ");
 		return result;
