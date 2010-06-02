c0af46b60da035dfdd3b117838ce7d371522b842
bfelipee <bfelipee@0be835e6-de77-11de-b357-fb893c5a43fa>
2010-06-02 13:15:38 +0000

    adding files back

---
+++ dirigent/src/org/dirigent/pdi/job/dirigent/DirigentPluginData.java
@@ -0,0 +1,15 @@
+package org.dirigent.pdi.job.dirigent;
+
+import org.pentaho.di.core.row.RowMetaInterface; 
+import org.pentaho.di.trans.step.BaseStepData;
+import org.pentaho.di.trans.step.StepDataInterface;
+
+public class DirigentPluginData extends BaseStepData implements StepDataInterface
+{
+	public RowMetaInterface outputRowMeta;
+
+    public DirigentPluginData()
+	{
+		super();
+	}
+}
