2b1a2d6b9afe0443ee35d053d57bf4964b1348da
karel.hubl@seznam.cz <karel.hubl@seznam.cz@0be835e6-de77-11de-b357-fb893c5a43fa>
2010-11-19 18:19:00 +0000

    Share project "dirigent-metafacade" into "https://dirigent.googlecode.com/svn"

---
+++ trunk/dirigent-metafacade/src/org/dirigent/metafacade/impl/ClassImpl.java
@@ -0,0 +1,28 @@
+/*
+ *  This file is part of Dirigent - the MDA generator.
+ *  Copyright (C) 2010  Karel Hubl http://dirigent.googlecode.com
+ *
+ *  Dirigent is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Dirigent is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU LesservGeneral Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Dirigent.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.dirigent.metafacade.impl;
+
+/**
+ * Default implementation of {@link org.dirigent.metafacade.IClass}.
+ * @author Karel Hubl
+ *
+ */
+public class ClassImpl extends AbstractClass  {
+}
+
+
