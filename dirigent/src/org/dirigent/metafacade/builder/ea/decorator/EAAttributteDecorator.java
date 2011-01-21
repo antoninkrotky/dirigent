26e6a762cd3c4201611b8c26227ac7282899d1eb
karel.hubl <karel.hubl@seznam.cz>
2011-01-21 19:14:35 +0000

    Added classifier property on IAttribute interface.

--- dirigent/src/org/dirigent/metafacade/builder/ea/decorator/EAAttributteDecorator.java
+++ dirigent/src/org/dirigent/metafacade/builder/ea/decorator/EAAttributteDecorator.java
@@ -1,12 +1,17 @@
 package org.dirigent.metafacade.builder.ea.decorator;
 
+import org.dirigent.metafacade.IElement;
+import org.dirigent.metafacade.builder.MetafacadeBuilder;
 import org.dirigent.metafacade.builder.decorator.AttributeDecorator;
 import org.dirigent.metafacade.builder.ea.dao.EAAttributeTagDAO;
+import org.dirigent.metafacade.builder.ea.dao.EAObjectDao;
 import org.dirigent.metafacade.builder.ea.vo.EAAttributeVO;
 import org.dirigent.metafacade.builder.vo.AttributeVO;
+import org.dirigent.metafacade.builder.vo.ObjectVO;
 
 public class EAAttributteDecorator extends AttributeDecorator {
 	private EAAttributeVO eaAttribute;
+	private IElement classifier;
 	public EAAttributteDecorator(EAAttributeVO ea) {
 		super(init(ea,new AttributeVO()));
 		eaAttribute=ea;
@@ -25,5 +30,27 @@ public class EAAttributteDecorator extends AttributeDecorator {
 		v.properties=new EAAttributeTagDAO().getObjectProperties(v.id);		
 		v.initialValue=ea.initialValue;
 		return v;
+	}
+	
+	
+	
+	/* (non-Javadoc)
+	 * @see org.dirigent.metafacade.builder.decorator.AttributeDecorator#getClassifier()
+	 */
+	@Override
+	public IElement getClassifier() {
+		//We need one more query to get classifier URI (ea_guid) so override lazy loading here.
+		if (classifier==null) {
+			if (eaAttribute.classifier>0) {
+				ObjectVO o=new EAObjectDao().getObjectById(eaAttribute.classifier);
+				if (o!=null) {
+					String classifierURI=o.uri;
+					classifier=MetafacadeBuilder.getMetafacadeBuilder().getMetafacade(classifierURI);
+				}
+			} else {
+				
+			}
+		}
+		return classifier;
 	}
 }
