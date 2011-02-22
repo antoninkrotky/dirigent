af11a41049fff41842d57f286f3d494cd8d9674e
karel.hubl@seznam.cz <karel.hubl@seznam.cz@0be835e6-de77-11de-b357-fb893c5a43fa>
2011-02-22 17:47:21 +0000

    Added EABIColumnDecorator.

--- trunk/dirigent/src/org/dirigent/metafacade/builder/ea/decorator/EADimensionColumnDecorator.java
+++ trunk/dirigent/src/org/dirigent/metafacade/builder/ea/decorator/EADimensionColumnDecorator.java
@@ -1,29 +1,19 @@
 package org.dirigent.metafacade.builder.ea.decorator;
 
 import org.dirigent.metafacade.IDimensionColumn;
-import org.dirigent.metafacade.IDomain;
 import org.dirigent.metafacade.builder.ea.vo.EAAttributeVO;
 
-public class EADimensionColumnDecorator extends EAColumnDecorator implements IDimensionColumn {
-	private IDomain domain;
+public class EADimensionColumnDecorator extends EABIColumnDecorator implements IDimensionColumn {
+
+	/**
+	 * 
+	 */
 	public EADimensionColumnDecorator(EAAttributeVO ea) {
 		super(ea);
-		domain=(IDomain)getClassifier();
 	}
 
 	@Override
 	public String getHistoryType() {
 		return getProperties().get("scdColumnType");
 	}
-	
-	@Override
-	public String getDataType() {
-		return domain.getDataType();
-	}
-
-	@Override
-	public IDomain getDomain() {
-		return domain;
-	}
-
 }
