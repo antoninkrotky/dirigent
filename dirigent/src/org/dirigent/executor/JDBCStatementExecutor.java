89bdb1df41d8529d6789c0015308de0ac3361441
bgrekov <bgrekov@gmail.com>
2011-04-04 11:42:10 +0000

    Added support for JDBCStatement pattern step parameters : executionType and schema.

--- dirigent/src/org/dirigent/executor/JDBCStatementExecutor.java
+++ dirigent/src/org/dirigent/executor/JDBCStatementExecutor.java
@@ -10,6 +10,7 @@ import java.util.logging.Logger;
 import org.dirigent.metafacade.IGeneratable;
 import org.dirigent.metafacade.ISchema;
 import org.dirigent.metafacade.ISchemaProvider;
+import org.dirigent.metafacade.builder.MetafacadeBuilder;
 import org.dirigent.pattern.IPatternStep;
 
 public class JDBCStatementExecutor implements IStepExecutor {
@@ -22,26 +23,44 @@ public class JDBCStatementExecutor implements IStepExecutor {
 			throw new IllegalArgumentException(
 					"Instance of ISchemaProvider is required in argument gen.");
 		}
-		Connection c = getConnection(((ISchemaProvider) gen).getSchema());
+
+		ISchema schema;
+
+		if (step.getParameter("schema") != null) {
+			schema = (ISchema) MetafacadeBuilder.getMetafacadeBuilder()
+					.getMetafacade(step.getParameter("schema"));
+		} else {
+			schema = ((ISchemaProvider) gen).getSchema();
+		}
+
+		Connection c = getConnection(schema);
 		// set autocommit to false
 		try {
 			c.setAutoCommit(false);
 		} catch (SQLException e1) {
-			l
-					.log(
-							Level.WARNING,
-							"Unable to set autocommit to false. Check your DB settings.",
-							e1);
+			l.log(Level.WARNING,
+					"Unable to set autocommit to false. Check your DB settings.",
+					e1);
 		}
 		String sql = TemplateHelper.generateTemplate(gen, step);
 		try {
 			PreparedStatement stmt = c.prepareStatement(sql);
 			l.info("Executing: " + sql);
-			int rowsAffected = stmt.executeUpdate();
-			stmt.close();
-			c.commit();
-			PatternExecutionStatistics.getStepStatistics().lastElement()
-					.setAffectedRows(rowsAffected);
+			// Choose execution type based on optional parameter "executionType"
+			String executionType = step.getParameter("executionType");
+			if (executionType != null && "executeQuery".equals(executionType)) {
+				stmt.executeQuery();
+				stmt.close();
+				c.commit();
+			}
+			// default
+			else {
+				int rowsAffected = stmt.executeUpdate();
+				stmt.close();
+				c.commit();
+				PatternExecutionStatistics.getStepStatistics().lastElement()
+						.setAffectedRows(rowsAffected);
+			}
 		} catch (SQLException e) {
 			try {
 				c.rollback();
@@ -60,8 +79,8 @@ public class JDBCStatementExecutor implements IStepExecutor {
 	private Connection getConnection(ISchema schema) {
 		try {
 			Class.forName(schema.getJdbcDriver());
-			return DriverManager.getConnection(schema.getJdbcUrl(), schema
-					.getUsername(), schema.getPassword());
+			return DriverManager.getConnection(schema.getJdbcUrl(),
+					schema.getUsername(), schema.getPassword());
 		} catch (Exception e) {
 			throw new RuntimeException(
 					"Unable to create connection for schema URI: "
