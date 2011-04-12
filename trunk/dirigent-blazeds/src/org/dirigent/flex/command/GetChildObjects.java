d05812072453cc48096ba4acf86a63d205868144
karel.hubl@seznam.cz <karel.hubl@seznam.cz@0be835e6-de77-11de-b357-fb893c5a43fa>
2011-04-12 07:26:16 +0000

    Added sorting of returned child objects.

--- trunk/dirigent-blazeds/src/org/dirigent/flex/command/GetChildObjects.java
+++ trunk/dirigent-blazeds/src/org/dirigent/flex/command/GetChildObjects.java
@@ -1,9 +1,10 @@
 package org.dirigent.flex.command;
 
-
-
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
+import java.util.List;
 
 import org.dirigent.config.DirigentConfig;
 import org.dirigent.flex.ICommand;
@@ -15,8 +16,6 @@ import org.dirigent.metafacade.builder.vo.ObjectVO;
 public class GetChildObjects implements ICommand {
 	public ObjectVO object;
 
-
-	
 	@Override
 	public Object execute() {
 		String uri;
@@ -26,23 +25,33 @@ public class GetChildObjects implements ICommand {
 		} else {
 			uri = object.uri;
 		}
-
+		List<ObjectVO> res;
 		if (uri != null) {
 			IElement element = MetafacadeBuilder.getMetafacadeBuilder()
 					.getMetafacade(uri);
 			if (element instanceof IComposite) {
-				return cleanCollection(((IComposite) element)
-						.getChildElements());
+				res = cleanCollection(((IComposite) element).getChildElements());
 			} else {
 				return null;
 			}
-		} 
-		
-			return cleanCollection(MetafacadeBuilder.getMetafacadeBuilder().getChildElements(uri));
-		
+		} else {
+			res = cleanCollection(MetafacadeBuilder.getMetafacadeBuilder()
+					.getChildElements(uri));
+		}
+		Collections.sort(res, new Comparator<ObjectVO>() {
+			@Override
+			public int compare(ObjectVO o1, ObjectVO o2) {
+				if (o1.name == null) {
+					return -1;
+				}
+				return o1.name.compareTo(o2.name);
+			}
+		});
+		return res;
+
 	}
 
-	private Collection<ObjectVO> cleanCollection(Collection<IElement> c) {
+	private List<ObjectVO> cleanCollection(Collection<IElement> c) {
 		Iterator<IElement> i = c.iterator();
 		java.util.ArrayList<ObjectVO> res = new java.util.ArrayList<ObjectVO>();
 		while (i.hasNext()) {
